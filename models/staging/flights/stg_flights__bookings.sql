6a960a9c3a3f06b015f85d8c57d56051c2251db1
Mrx112426 <victor.dunduk@mail.ru>
2025-12-23 18:19:07 +0300

    Обновление таблиц staging слоя

--- models/staging/flights/stg_flights__bookings.sql
+++ models/staging/flights/stg_flights__bookings.sql
@@ -1,11 +1,16 @@
 {{
-  config(
-	materialized = 'table'
-	)
+	config(
+		materialized = 'incremental',
+		incremental_strategy = 'append'
+)
 }}
 select 
-    book_ref,
+	book_ref,
 	book_date,
 	total_amount
 from 
 	{{ source('demo_src', 'bookings')}}
+{% if is_incremental() %}
+where 
+	book_ref > (select max(book_ref) from {{this}})
+{% endif %}
